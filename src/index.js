591b4b27c8b93377f97fa1229cf7542db0acc34d
Andy Chen <andy@Andys-MacBook-Pro.local>
2019-03-20 18:45:22 -0700

    added collision and camera. still need to fix bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "phaser";
 
-const config = {
+var config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
   width: 800,
@@ -18,9 +18,9 @@ const config = {
   }
 };
 
-const game = new Phaser.Game(config);
+var game = new Phaser.Game(config);
 let player;
-let cursors;
+var cursors;
 
 
 
@@ -37,19 +37,62 @@ function create() {
   const tileset = map.addTilesetImage("assets", "tiles");
 
   this.add.image(600, 300, 'background');
+
   const lowerLayer = map.createStaticLayer("LowerGround", tileset, 0, 0);
   const groundLayer = map.createStaticLayer("Ground", tileset, 0, 0);
   const grassLayer = map.createStaticLayer("Grass", tileset, 0, 0);
   const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
   const highLayer = map.createStaticLayer("High", tileset, 0, 0);
 
+  lowerLayer.setCollisionByProperty({ collides: true });
+  groundLayer.setCollisionByProperty({ collides: true });
+  worldLayer.setCollisionByProperty({ collides: true });
+  highLayer.setDepth(10);
+
   player = this.physics.add.sprite(100, 450, 'player');
+  this.physics.add.collider(player, lowerLayer)
+  this.physics.add.collider(player, belowLayer)
+  this.physics.add.collider(player, worldLayer)
+
+  const anims = this.anims;
+  anims.create({
+    key: "left",
+    frames: anims.generateFrameNames("player", { start: 20, end: 29 }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: "right",
+    frames: anims.generateFrameNames("player", { start: 30, end: 39 }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: "front",
+    frames: anims.generateFrameNames("player", { start: 0, end: 7 }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: "back",
+    frames: anims.generateFrameNames("player", { start: 10, end: 17 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  const camera = this.cameras.main
+  camera.startFollow(player)
+  camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
 }
 
 
 function update() {
+  const prevVelocity = player.body.velocity.clone();
+  
+  player.body.setVelocity(0);
   cursors = this.input.keyboard.createCursorKeys();
   
+
   if (cursors.left.isDown) {
     player.body.setVelocityX(-100);
   } else if (cursors.right.isDown) {
@@ -59,4 +102,22 @@ function update() {
   } else if (cursors.down.isDown) {
     player.body.setVelocityY(100);
   }
+
+  if (cursors.left.isDown) {
+    player.anims.play("left", true);
+  } else if (cursors.right.isDown) {
+    player.anims.play("right", true);
+  } else if (cursors.up.isDown) {
+    player.anims.play("back", true);
+  } else if (cursors.down.isDown) {
+    player.anims.play("front", true);
+  } else {
+    player.anims.stop();
+
+    if (prevVelocity.x < 0) player.setTexture('player', 'left')
+      else if (prevVelocity.x > 0) player.setTexture('player', 'right')
+      else if (prevVelocity.y < 0) player.setTexture('player', 'back')
+      else if (prevVelocity.y > 0) player.setTexture('player', 'front')
+  }
+  
 }
